eb37266a687039889b97c2a5844aab6b55b64960
Travis Berry <contact@travisberry.com>
2015-01-31 23:22:45 -0700

    docker test'

--- main.go
+++ main.go
@@ -20,10 +20,17 @@ func main() {
 	r.HandleFunc(ConUrl+"json", jsonHandler).Methods(ConMethod)
 	r.HandleFunc(ConUrl+"html", htmlHandler).Methods(ConMethod)
 	r.HandleFunc(ConUrl+"image", imageHandler).Methods(ConMethod)
+	r.HandleFunc("/user/{name:[a-z]+}/profile", profile).Methods("GET")
 
 	http.Handle(ConRootUrl, r)
 
 	LogInfo("Listening...")
 	handler := http.TimeoutHandler(SetHeaders(Logger(rateLimit(r))), time.Second*25, "Timeout!")
 	LogFatal(graceful.ListenAndServe(ConPort, handler))
+}
+
+func profile(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	name := params["name"]
+	w.Write([]byte("Hello " + name + " ENV VAR IS: " + os.Getenv("DEIS_TEST_VAR")))
 }
